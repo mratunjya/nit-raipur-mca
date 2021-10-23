87c4c7fd8ac097272698c461b0220f511eb1548e
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2021-10-24 01:14:33 +0530

    Update syllabus.js

--- Semester 1/JavaScript/syllabus.js
+++ Semester 1/JavaScript/syllabus.js
@@ -1,9 +1,12 @@
 let syllabus = document.getElementById("syllabus");
+let body = document.getElementsByTagName("body")[0];
 
 function syllabus_show () {
     syllabus.classList.toggle("show");
+    body.classList.toggle("no-overflow");
 }
 
 function syllabus_hide () {
     syllabus.classList.toggle("show");
+    body.classList.toggle("no-overflow");
 }
