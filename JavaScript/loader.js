918b5d5a4909efc14b4b2c93483e60c816eca4d6
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2021-12-04 12:17:57 +0000

    Major UI Update

--- JavaScript/loader.js
+++ JavaScript/loader.js
@@ -1,3 +1,6 @@
 $(window).on("load",function(){
     $(".loader").fadeOut("slow");
+    setTimeout(function(){
+        document.querySelector("body").classList.toggle("overflow");
+    }, 250)
 });
--- Semester 1/JavaScript/time-table.js
+++ Semester 1/JavaScript/time-table.js
@@ -10,3 +10,16 @@ function time_table_hide() {
     time_table.classList.toggle("show");
     body.classList.toggle("no-overflow");
 }
+
+function time_table_btn() {
+    document.querySelector(".time-table-btn").style.display = "none";
+    setTimeout(function() {
+        document.querySelector(".time-table-btn").style.display = "block";
+    }, 250);
+}
+
+window.addEventListener("scroll", time_table_btn);
+document.querySelector(".class-links").addEventListener("scroll", time_table_btn);
+document.querySelector(".attendance-links").addEventListener("scroll", time_table_btn);
+document.querySelector(".books").addEventListener("scroll", time_table_btn);
+document.querySelector(".Important-msg").addEventListener("scroll", time_table_btn);
