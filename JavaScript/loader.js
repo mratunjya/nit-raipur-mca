24ab66b65884dfee7fcfedcf7678612180dd112e
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2021-12-09 10:18:49 +0000

    X button is sticky now

--- JavaScript/loader.js
+++ JavaScript/loader.js
@@ -1,6 +1,9 @@
-$(window).on("load",function(){
+$(window).on("load", function() {
     $(".loader").fadeOut("slow");
-    setTimeout(function(){
-        document.querySelector("body").classList.toggle("overflow");
+    setTimeout(function() {
+        //If classList of body contains overflow then only toggle overflow class
+        if (!document.body.classList.contains("overflow")) {
+            document.body.classList.toggle("overflow");
+        }
     }, 250)
 });
