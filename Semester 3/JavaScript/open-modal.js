64012a0aa8fbecc04b3cfa4447637b5d8097c5d2
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2022-07-05 23:49:00 +0530

    Semester 3 is Added, Lot of Code cleanup and code styling (#2)

---
+++ Semester 3/JavaScript/open-modal.js
@@ -0,0 +1,38 @@
+const noScroll = () => {
+  document.body.style.overflow = "hidden";
+};
+
+const scroll = () => {
+  document.body.style.overflow = "auto";
+};
+
+const scrollModalTop = () => {
+  document.querySelector(".modal-body").scrollTo(0, 0);
+};
+
+const windowScrollTop = () => {
+  window.scrollTo(0, 0);
+};
+
+allClicks = document.querySelectorAll(".open-modal");
+const modalContainer = document.querySelector(".modal-container");
+const modalContainerImg = document.querySelector(".modal-body img");
+
+allClicks.forEach(function (item) {
+  item.addEventListener("click", function () {
+    modalContainerImg.src = `png/${this.dataset.src}`;
+    scrollModalTop();
+    windowScrollTop();
+    noScroll();
+    modalContainer.classList.toggle("is-flex");
+  });
+});
+
+modalContainer.addEventListener("click", function () {
+  this.classList.toggle("is-flex");
+  scroll();
+});
+
+modalContainerImg.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
