a8d5b75880291454db9c06597954441cea7991dc
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2022-09-01 17:05:41 +0530

    Extra layer of security for Python Notes

--- JavaScript/master.js
+++ JavaScript/master.js
@@ -1,5 +1,6 @@
 window.onkeydown = (e) => {
   if (e.ctrlKey && e.altKey && e.shiftKey && (e.key == "p" || e.key == "P")) {
+    sessionStorage.setItem("pythonNotes", true);
     window.location.href = "/Python-Notes";
   }
 };
