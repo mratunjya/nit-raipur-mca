11ec968bef576447b2effd4fe8d0d1703bd894d5
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2022-12-27 19:25:17 +0530

    Added Sem 4 Syllabus PDF

--- Semester 4/JavaScript/open-modal.js
+++ Semester 4/JavaScript/open-modal.js
@@ -17,10 +17,24 @@ const windowScrollTop = () => {
 allClicks = document.querySelectorAll(".open-modal");
 const modalContainer = document.querySelector(".modal-container");
 const modalContainerImg = document.querySelector(".modal-body img");
+const modalContainerPdf = document.querySelector("#pdf-viewer");
 
 allClicks.forEach(function (item) {
   item.addEventListener("click", function () {
-    modalContainerImg.src = `${this.dataset.ext}/${this.dataset.src}.${this.dataset.ext}`;
+    const extension = this.dataset.ext;
+    console.log(extension);
+    if (extension === "pdf") {
+      modalContainerImg.style.display = "none";
+      modalContainerPdf.style.display = "block";
+      PDFObject.embed(
+        `${extension}/${this.dataset.src}.${this.dataset.ext}#scrollbar=0&toolbar=1`,
+        "#pdf-viewer"
+      );
+    } else if (extension === "jpg" || extension === "png") {
+      modalContainerImg.style.display = "block";
+      modalContainerPdf.style.display = "none";
+      modalContainerImg.src = `${extension}/${this.dataset.src}.${this.dataset.ext}`;
+    }
     scrollModalTop();
     windowScrollTop();
     noScroll();
